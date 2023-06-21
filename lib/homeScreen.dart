fc5cbe5e54642e1b696e7c1f41e8a7f4550292ce
ukantjadia <ukantjadia0120@gmail.com>
2023-06-21 23:51:35 +0530

    data added

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -1,9 +1,23 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 import 'GPScehcker/gpsChecker.dart';
 
 class homePage extends StatelessWidget {
-  const homePage({super.key});
+  homePage({super.key});
+  
+
+  
+  Future getData() async {
+    var response = await http.get(Uri.https('api.openweathermap.org','/data/2.5/forecast/daily'.{'lat':lan,}));
+    var jsonData = jsonDecode(response.body);
+    print(jsonData);
+    // api.openweathermap.org/data/2.5/forecast/daily?lat={lat}&lon={lon}&cnt={cnt}&appid={API key}
+
+
+  }
 
   @override
   Widget build(BuildContext context) {
